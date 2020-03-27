3bad5188a3c50cb88e60088405b24e4b5a7d4ca0
Diego Benjamín Aguilar Aguilar <diegoaguilaraguilar@gmail.com>
2020-03-26 22:04:50 -0600

    We can dismiss async npm module and use ES6 async/await

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const Airtable = require('airtable');
 const { Client } = require('pg');
-var async = require("async");
 
 //setup airtable connection
 const base = new Airtable({
